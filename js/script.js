15138606702e6a5f27781249cf7a0c83bf971d03
hafiz4546 <hafiz4546376@gmail.com>
2021-04-16 17:19:25 +0600

    update

--- js/script.js
+++ js/script.js
@@ -66,11 +66,11 @@
 // }
 
 
-const inp = document.querySelector('.input-operand')
-const out = document.querySelector('.output-operand')
+const inp = document.querySelector('.display1')
+const out = document.querySelector('.display2')
 const buttonEl = document.querySelectorAll('.button')
 const operationEl = document.querySelectorAll('.operation')
-const cleaarEl = document.querySelector('.clear')
+const clearEl = document.querySelector('.clear')
 const deletEl = document.querySelector('.delet')
 const equalEl = document.querySelector('.equal')
 
@@ -79,15 +79,71 @@ let input = '';
 let output = '';
 let haveDot = false;
 let lastOperation = '';
+let result = null;
 
 buttonEl.forEach(button => {
     button.addEventListener('click',(e) => {
       if(e.target.textContent === '.' && !haveDot){
           haveDot = true;
       }else if(e.target.textContent === '.' && haveDot){
-          return;
+          return; 
       }
       input += e.target.textContent;
       inp.textContent = input
     })
+});
+
+
+// operationEl.forEach(operation => {
+//     operation.addEventListener('click', (e) =>{
+//         if(!input) return;
+//         haveDot = false;
+//         const operationName = e.target.textContent
+//         if(input && lastOperation){
+//             mathOperation()
+//         } else{
+//             result = parseFloat(output)
+//         }
+//         console.log(result);
+//         clearVar(operationName)
+//         lastOperation = operationName;
+//     })
+// })
+
+
+
+// function mathOperation(){
+//     if(lastoperation === '%'){
+//         result = parseFloat(result) % parseFloat(input)
+//     } else if(lastOperation === '+'){
+//         result = parseFloat(result) + parseFloat(input)
+//     } else if(lastOperation === '-'){
+//         result = parseFloat(result) - parseFloat(input)
+//     } else if(lastOperation === '*'){
+//         result = parseFloat(result) * parseFloat(input)
+//     }  else if(lastOperation === '÷'){
+//         result = parseFloat(result) / parseFloat(input)
+//     }    
+// }
+
+// equalEl.addEventListener('click',(e) => {
+//     if(!input) return;
+//     haveDot = false;
+//     mathOperation();
+//     out.textContent = result;
+//     output = result;
+//     input = '';
+// })
+
+clearEl.addEventListener('click',(e) => {
+    inp.textContent = '';
+    out.textContent = '';
+    input = '';
+    output = '';
+    result = '';
+});
+
+deletEl.addEventListener('click',(e) => {
+    inp = !result
+    inp.textContent = result[result.length-1]
 })
